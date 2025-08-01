ae3a36664f93629ed520044e43cfcc06d8c1b940
Yamisipq <rosasyamith@gmail.com>
2025-08-01 00:06:47 -0500

    Primeros ejercicios, revision!.

---
+++ Calculadora IMC.py
@@ -0,0 +1,7 @@
+n1=float(input("Digite su peso en kg"))
+n2=float(input("Digite su altura en metros"))
+
+""" Pedimos al usuario que ingrese sus datos y los multiplicamos, añademos la funcion .2f 
+para que este redondeado a dos decimales """
+
+print(f"Su IMC es: {n1*n2:.2f}")
---
+++ Verificador +18.py
@@ -0,0 +1,10 @@
+edad=int(input("Digite su edad:"))
+
+if edad <= 18:
+    print("Usted es menor de edad")
+
+elif edad > 18 and edad <= 25:
+    print("Joven adulto")
+
+else:
+    print("Usted es mayor de 18")

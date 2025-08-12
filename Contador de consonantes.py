76e957cc4799e1b68abe1fbbb95206127804ba2d
Yamith Rosas <rosasyamith@gmail.com>
2025-08-12 11:38:15 -0500

    MEJORA DE EJ3.C DE CONSONANTES

--- Contador de consonantes.py
+++ Contador de consonantes.py
@@ -7,21 +7,21 @@ def contador_consonantes_vocal(frase: str) -> None:
     Returns:
         Valor consonantes y vocales
     """
-    vocal = "aeiou"
+    vocales = "aeiou"
+    if not frase.isalpha():
+        raise ValueError("La frase solo debe contener letras (sin espacios, números ni caracteres especiales).")
 
-    cv=0
-    cc=0
+    cv = 0
+    cc = 0
 
     for i in frase:
-        if i in vocal:
-         cv += 1
+        if i in vocales:
+            cv += 1
         else:
-             cc += 1
+            cc += 1
 
+    print(f"Número de vocales en {frase} es {cv}")
+    print(f"Número de consonantes en {frase} es {cc}")
 
-    print(f"Numero de vocales en {frase} es {cv}")
-    print(f"Numero de consonantes en {frase} es {cc}")
-
-frase : str.lower=(input("Ingresa una frase: "))
-frase = frase.replace(" ","")
+frase = input("Ingresa una frase: ").lower().replace(" ", "")
 contador_consonantes_vocal(frase)
